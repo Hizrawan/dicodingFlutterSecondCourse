807fdbbd9486b5ac462544ccb600a062cc29ca69
hizrawan <hizrawandwioka@gmail.com>
2025-08-29 13:38:57 +0800

    learn about app and widget lifecycle

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant_app/screen/home/home.dart';
+import 'package:restaurant_app/stateful_widget.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  State<MyApp> createState() => _myAppState();
+}
+
+class _myAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +22,28 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomeScreen(),
+      home: 
+      //HomeScreen(), //restaurant app
+      const MyStatefulWidget() // untuk latihan lifecycle
     );
   }
+
+ late final AppLifecycleListener _listener;
+
+ @override
+ void initstate(){
+  super.initState();
+  _listener = AppLifecycleListener(
+    onDetach: () => debugPrint("AppLifecycleState.detached"),
+    onInactive: () => debugPrint("AppLifecycleState.inactive"),
+    onPause: () => debugPrint("AppLifecycleState.paused"),
+    onResume: () => debugPrint("AppLifecycleState.resumed"),
+    onRestart: () => debugPrint("AppLifecycleState.restarted")
+    );
+ }
+ @override
+ void dispose(){
+  _listener.dispose();
+  super.dispose();
+ }
 }
