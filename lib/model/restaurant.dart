695722236e4d263da821266e097caae0041d9409
Hizrawan <hizrawandwioka@gmail.com>
2025-08-30 07:50:49 +0800

    Learn constrain for widget

--- lib/model/restaurant.dart
+++ lib/model/restaurant.dart
@@ -26,7 +26,7 @@ final List<Restaurant> restaurantList = [
         'Taman Nasional Kelimutu terletak di Flores, Indonesia. Taman nasional ini terdiri dari bukit-bukit dan gunung-gunung dengan Gunung Kelibara (1.731 m) sebagai puncak tertinggi. Gunung Kelimutu, terdapat danau Danau tiga warna yang juga merupakan tempat dari Taman Nasional Kelimutu.\n\nDi dalam Taman Nasional Kelimutu, terdapat arboretum, hutan kecil seluas 4,5 hektare yang mewakili koleksi keanekaragaman flora di daerah tersebut. Di sana terdapat 78 jenis pohon yang dikelompokkan ke dalam 36 suku. Beberapa koleksi flora yang merupakan endemik Kelimutu adalah uta onga (Begonia kelimutuensis), turuwara (Rhododendron renschianum), dan arngoni (Vaccinium varingiaefolium).',
     city: 'Detusoko, Kabupaten Ende, NTT',
     
-    pictureId: 1,
+    pictureId: 11,
     rating:3),
   Restaurant(
     id: 2,
@@ -35,9 +35,8 @@ final List<Restaurant> restaurantList = [
         'Danau Toba adalah lokasi letusan gunung berapi super masif berkekuatan VEI 8 sekitar 69.000 sampai 77.000 tahun yang lalu yang memicu perubahan iklim global. Metode penanggalan terkini menetapkan bahwa 74.000 tahun yang lalu lebih akurat. Letusan ini merupakan letusan eksplosif terbesar di Bumi dalam kurun 25 juta tahun terakhir. Menurut teori bencana Toba, letusan ini berdampak besar bagi populasi manusia di seluruh dunia; dampak letusan menewaskan sebagian besar manusia yang hidup waktu itu dan diyakini menyebabkan penyusutan populasi di Afrika timur tengah dan India sehingga memengaruhi genetika populasi manusia di seluruh dunia sampai sekarang.',
     city: 'Kota Pematang Siantar, Sumatera Utara',
     
-    pictureId: 2,
-    rating:
-        3),
+    pictureId: 12,
+    rating:3),
   Restaurant(
     id: 3,
     name: 'Gunung Bromo',
@@ -45,9 +44,8 @@ final List<Restaurant> restaurantList = [
         'Gunung Bromo (dari bahasa Sanskerta: Brahma, salah seorang Dewa Utama dalam agama Hindu) atau dalam bahasa Tengger dieja "Brama", adalah sebuah gunung berapi aktif di Jawa Timur, Indonesia. Gunung ini memiliki ketinggian 2.329 meter di atas permukaan laut dan berada dalam empat wilayah kabupaten, yakni Kabupaten Probolinggo, Kabupaten Pasuruan, Kabupaten Lumajang, dan Kabupaten Malang. Gunung Bromo terkenal sebagai objek wisata utama di Jawa Timur. Sebagai sebuah objek wisata, Bromo menjadi menarik karena statusnya sebagai gunung berapi yang masih aktif. Gunung Bromo termasuk dalam kawasan Taman Nasional Bromo Tengger Semeru.\n\nBentuk tubuh Gunung Bromo bertautan antara lembah dan ngarai dengan kaldera atau lautan pasir seluas sekitar 10 kilometer persegi. Ia mempunyai sebuah kawah dengan garis tengah ± 800 meter (utara-selatan) dan ± 600 meter (timur-barat). Sedangkan daerah bahayanya berupa lingkaran dengan jari-jari 4 km dari pusat kawah Bromo.',
     city: 'Podokoyo, Tosari, Pasuruan',
     
-    pictureId: 3,
-    rating:
-        3 ),
+    pictureId: 13,
+    rating:3 ),
   Restaurant(
     id: 4,
     name: 'Bunaken',
@@ -55,7 +53,7 @@ final List<Restaurant> restaurantList = [
         'Bunaken adalah sebuah pulau seluas 8,08 km² di Teluk Manado, yang terletak di utara pulau Sulawesi, Indonesia. Pulau ini merupakan bagian dari kota Manado, ibu kota provinsi Sulawesi Utara, Indonesia. Pulau Bunaken dapat di tempuh dengan kapal cepat (speed boat) atau kapal sewaan dengan perjalanan sekitar 30 menit dari pelabuhan kota Manado. Di sekitar pulau Bunaken terdapat taman laut Bunaken yang merupakan bagian dari Taman Nasional Bunaken. Taman laut ini memiliki biodiversitas kelautan salah satu yang tertinggi di dunia. Selam scuba menarik banyak pengunjung ke pulau ini. Secara keseluruhan taman laut Bunaken meliputi area seluas 75.265 hektare dengan lima pulau yang berada di dalamnya, yakni Pulau Manado Tua (Manarauw), Pulau Bunaken, Pulau Siladen, Pulau Mantehage berikut beberapa anak pulaunya, dan Pulau Naen. Meskipun meliputi area 75.265 hektare, lokasi penyelaman (diving) hanya terbatas di masing-masing pantai yang mengelilingi kelima pulau itu.',
     city: 'Bunaken, Kabupaten Minahasa, Sulawesi Utara.',
     
-    pictureId: 4,
+    pictureId: 14,
     rating:3),
   Restaurant(
     id: 5,
@@ -64,18 +62,16 @@ final List<Restaurant> restaurantList = [
         'Pulau Komodo adalah sebuah pulau yang terletak di Kepulauan Nusa Tenggara. Pulau Komodo dikenal sebagai habitat asli hewan komodo. Pulau ini juga merupakan kawasan Taman Nasional Komodo yang dikelola oleh Pemerintah Pusat. Pulau Komodo berada di sebelah timur Pulau Sumbawa, yang dipisahkan oleh Selat Sape. Di Pulau Komodo, hewan komodo hidup dan berkembang biak dengan baik. Hingga Agustus 2009, di pulau ini terdapat sekitar 1300 ekor komodo. Ditambah dengan pulau lain, seperti Pulau Rinca dan dan Gili Motang, jumlah mereka keseluruhan mencapai sekitar 2500 ekor. Ada pula sekitar 100 ekor komodo di Cagar Alam Wae Wuul di daratan Pulau Flores tapi tidak termasuk wilayah Taman Nasional Komodo.',
     city: 'Kabupaten Manggarai Barat, NTT',
     
-    pictureId: 5,
-    rating:
-       3 ),
+    pictureId: 15,
+    rating: 3 ),
   Restaurant(
     id: 6,
     name: 'Dieng Plateau',
     description:
         'Dataran tinggi Dieng (DTD) adalah dataran dengan aktivitas vulkanik di bawah permukaannya, seperti Yellowstone ataupun Dataran Tinggi Tengger. Sesungguhnya ia adalah kaldera dengan gunung-gunung di sekitarnya sebagai tepinya. Terdapat banyak kawah sebagai tempat keluarnya gas, uap air dan berbagai material vulkanik lainnya. Keadaan ini sangat berbahaya bagi penduduk yang menghuni wilayah itu, terbukti dengan adanya bencana letusan gas Kawah Sinila 1979. Tidak hanya gas beracun, tetapi juga dapat dimungkinkan terjadi gempa bumi, letusan lumpur, tanah longsor, dan banjir.',
     city: 'Batur, Banjarnegara, Jawa Tengah',
-    pictureId: 6,
-    rating:
-        3),
+    pictureId: 16,
+    rating:3),
   Restaurant(
     id: 7,
     name: 'Kepulauan Raja Ampat',
@@ -84,7 +80,7 @@ final List<Restaurant> restaurantList = [
     city: 'Kabupaten Raja Ampat, Papua Barat',
     
     
-    pictureId: 7,
+    pictureId: 17,
     rating:3 ),
   Restaurant(
     id: 8,
@@ -93,7 +89,7 @@ final List<Restaurant> restaurantList = [
         'Kepulauan Derawan adalah sebuah kepulauan yang berada di Kabupaten Berau, Kalimantan Timur. Di kepulauan ini terdapat sejumlah objek wisata bahari menawan, salah satunya Taman Bawah Laut yang diminati wisatawan mancanegara terutama para penyelam kelas dunia.\n\nKepulauan Derawan memiliki tiga kecamatan yaitu, Pulau Derawan, Maratua, dan Biduk Biduk, Berau.\n\nSedikitnya ada empat pulau yang terkenal di kepulauan tersebut, yakni Pulau Maratua, Derawan, Sangalaki, dan Kakaban yang ditinggali satwa langka penyu hijau dan penyu sisik.',
     city: 'Kabupaten Berau, Kalimantan Timur',
     
-    pictureId: 8,
+    pictureId: 18,
     rating:3),
   Restaurant(
     id: 9,
@@ -103,7 +99,7 @@ final List<Restaurant> restaurantList = [
     city: 'Sugihmukti, Kec. Pasirjambu, Bandung',
     
    
-    pictureId: 9,
+    pictureId: 19,
     rating:3 ),
   Restaurant(
     id: 10,
@@ -112,9 +108,8 @@ final List<Restaurant> restaurantList = [
         'Keistimewaan dari pantai ini adalah batu granit yang beraneka ragam ukurannya, dari yang hanya beberapa meter kubik hingga ratusan meter kubik. Saat matahari mulai terbenam pantai tanjung tinggi akan memancarkan pesona kecantikaannya yang luar biasa. Dengan keindahan pantai yang memiliki khas tersendiri sehingga membuat pantai tanjung tinggi banyak dikenal oleh para wisatawan luar daerah maupun luar negeri. Pantai Tanjung Tinggi ini pernah dijadikan lokasi shuting film Laskar Pelangi dan Sang Pemimpi, jadi tak jarang para wisatawan luar menyebut pantai tanjung tinggi ini dengan sebutan pantai Laskar Pelangi.',
     city: 'Kecamatan Sijuk, Kabupaten Belitung, Bangka Belitung',
     
-    pictureId: 10,
-    rating:
-        3 ),
+    pictureId: 11,
+    rating: 3 ),
   Restaurant(
     id: 11,
     name: 'Banda Neira',
@@ -124,8 +119,7 @@ final List<Restaurant> restaurantList = [
     
     
     pictureId: 11,
-    rating:
-       3),
+    rating:3),
   Restaurant(
     id: 12,
     name: 'Air Terjun Wafsarak',
@@ -134,6 +128,5 @@ final List<Restaurant> restaurantList = [
     city: 'Biak Utara, Papua Barat',
     
     pictureId: 12,
-    rating:
-        3),
+    rating:3),
 ];
--- lib/screen/home/restaurant_card_widget.dart
+++ lib/screen/home/restaurant_card_widget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:restaurant_app/model/restaurant.dart';
 
@@ -5,40 +6,67 @@ class RestaurantCard extends StatelessWidget {
   final Restaurant restaurant;
   final Function()? onTap;
 
-  const RestaurantCard({
-    super.key,
-    required this.restaurant,
-    this.onTap,
-  });
+  const RestaurantCard({super.key, required this.restaurant, this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
       child: Padding(
-      padding: const EdgeInsets.symmetric(
-        vertical: 8,
-        horizontal: 16,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            restaurant.name,
-            style: const TextStyle(
-              fontSize: 16,
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxWidth: 120,
+                maxHeight: 120,
+                minHeight: 80,
+                minWidth: 80,
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: Image.network(restaurant.imageUrl, fit: BoxFit.cover),
+              ),
             ),
-          ),
-          Text(
-            restaurant.description,
-            maxLines: 3,
-            overflow: TextOverflow.ellipsis,
-            softWrap: true,
-          ),
-        ],
+            const SizedBox.square(dimension: 8),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(restaurant.name, style: const TextStyle(fontSize: 16)),
+                  const SizedBox.square(dimension: 6),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          restaurant.city,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          softWrap: true,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(
+                        Icons.star,
+                        size: 16,
+                        color: Color(0xFFFFC107),
+                      ),
+                      const SizedBox.square(dimension: 4),
+                      Expanded(child: Text(restaurant.rating.toString())),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-    ),);
+    );
   }
 }
