d8e785c3c58c403572c5da00cd95f3edf7ac0b48
Hizrawan <hizrawandwioka@gmail.com>
2025-08-31 11:18:24 +0800

    add search bar

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -3,19 +3,55 @@ import 'package:restaurant_app/model/restaurant.dart';
 import 'package:restaurant_app/screen/home/restaurant_card_widget.dart';
 import 'package:restaurant_app/static/NavigationRoute.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String query = "";
+
+  @override
   Widget build(BuildContext context) {
+    // filter list sesuai query
+    final filteredRestaurants = restaurantList.where((restaurant) {
+      return restaurant.name.toLowerCase().contains(query.toLowerCase());
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Restaurant List"),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(56),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "Search restaurant...",
+                prefixIcon: const Icon(Icons.search),
+                filled: true,
+                fillColor: Colors.white,
+                contentPadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 16),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  query = value;
+                });
+              },
+            ),
+          ),
+        ),
       ),
       body: ListView.builder(
-        itemCount: restaurantList.length,
+        itemCount: filteredRestaurants.length,
         itemBuilder: (context, index) {
-          final restaurant = restaurantList[index];
+          final restaurant = filteredRestaurants[index];
 
           return RestaurantCard(
             restaurant: restaurant,
